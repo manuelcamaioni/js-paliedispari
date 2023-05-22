b4d262116ac45989dea2d47ae511f6674f59e6ce
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-22 15:28:07 +0200

    palindrome check

--- script.js
+++ script.js
@@ -10,3 +10,19 @@
 // 1 - Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2 - Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 
+const insertedWord = prompt('Inserisci una parola o una frase.');
+
+function palindromeCheck(string){
+    const re = /[\W_]/g; // variabile che rimuove dalla stringa tutti i caratteri non alfanumerici da a a z / da A a Z/ da 0 a 9
+    const lowRegStr = string.toLowerCase().replace(re, ''); //creazione di un'altra variabile per inserire la stringa in lowercase e sostituire i caratteri speciali con ''
+    const reverseStr = lowRegStr.split('').reverse().join(''); // in un'altra variabile si splitta ogni carattere e si crea un array, si capovolgel'ordine e si riunisce il tutto in un unica stringa
+    return reverseStr === lowRegStr; //la funzione controlla se la variabile con la stringa invertita equivale a quella normale e restituisce un valore booleano 
+  }
+
+
+console.log(insertedWord);
+console.log(palindromeCheck(insertedWord));
+
+
+
+
