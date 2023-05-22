04fc543a24553a4b95bb5771898c3edb0114ec09
manuelcamaioni <camaioni.manuel@gmail.com>
2023-05-22 17:07:15 +0200

    tweaks

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ const insertedWord = prompt('Inserisci una parola o una frase.');
 
 function palindromeCheck(string){
     const re = /[\W_]/g; // variabile che rimuove dalla stringa tutti i caratteri non alfanumerici da a a z / da A a Z/ da 0 a 9
-    const lowRegStr = string.toLowerCase().replace(re, ''); //creazione di un'altra variabile per inserire la stringa in lowercase e sostituire i caratteri speciali con ''
-    const reverseStr = lowRegStr.split('').reverse().join(''); // in un'altra variabile si splitta ogni carattere e si crea un array, si capovolgel'ordine e si riunisce il tutto in un unica stringa
-    return reverseStr === lowRegStr; //la funzione controlla se la variabile con la stringa invertita equivale a quella normale e restituisce un valore booleano 
+    const lowRegStr = string.toLowerCase().replace(re, ''); // creazione di un'altra variabile per inserire la stringa in lowercase e sostituire i caratteri speciali con ''
+    const reverseStr = lowRegStr.split('').reverse().join(''); // in un'altra variabile si splitta ogni carattere e si crea un array, s'inverte l'ordine degli elementi e si riunisce il tutto in un'unica stringa
+    return reverseStr === lowRegStr; // la funzione controlla se la variabile con la stringa invertita equivale a quella normale e restituisce un valore booleano 
   }
 
 
@@ -67,6 +67,6 @@ function checkOddEven(totalSum){
     if(totalSum % 2 === 0){
         return 'Pari vince.';
     }
-    return 'Dispari vince.'
+    return 'Dispari vince.';
 }
 
